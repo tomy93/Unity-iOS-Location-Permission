49af1e41384c6af4ee85392baf9e9b344026b534
Tomy Tomás <tomy.tomas.talledo@gmail.com>
2024-09-19 21:57:44 -0500

    firts

---
+++ LocationPermissionStatus.mm
@@ -0,0 +1,22 @@
+#import <CoreLocation/CoreLocation.h>
+
+extern "C" {
+    int _getLocationPermissionStatus() {
+        int authorizationStatus = -1;
+        if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusNotDetermined) {
+            authorizationStatus = 0; // Not determined
+        } else if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusRestricted || [CLLocationManager authorizationStatus] == kCLAuthorizationStatusDenied) {
+            authorizationStatus = 1; // Denied or restricted
+        } else if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusAuthorizedWhenInUse || [CLLocationManager authorizationStatus] == kCLAuthorizationStatusAuthorizedAlways) {
+            authorizationStatus = 2; // Authorized
+        }
+        return authorizationStatus;
+    }
+
+    /*
+    // Method to access iOS settings
+    void _accessIOSSettings() {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString] options:@{} completionHandler:nil];
+    }
+    */
+}
